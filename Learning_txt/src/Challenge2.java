c144294eefd36d395107d4ae1f21efac8b9b08fb
Ishmeet Bindra <ibindra1995@gmail.com>
2018-03-03 05:33:00 +0530

    challenge2

--- Learning_txt/src/Challenge2.java
+++ Learning_txt/src/Challenge2.java
@@ -2,6 +2,14 @@
 public class Challenge2 {
 	
 	public static void main(java.lang.String []arg) {
+		int score = 1000;
+		int levelCompleted = 8;
+		int bonus = 200;
 		
+		if ((score >= 1000) && (levelCompleted == 8 )) {
+			System.out.println(score);
+			System.out.println("Bonus is :- " + bonus);
+			
+		}
 	}
 }
