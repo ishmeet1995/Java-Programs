fccb5585517ca9f81dd74bd4e4071a2adcb9243e
Ishmeet Bindra <ibindra1995@gmail.com>
2018-06-25 12:25:53 +0530

    6/25/2018

---
+++ Java_May_5/src/practice_clas.java
@@ -0,0 +1,19 @@
+class BOX{
+	int height = 2;
+	int depth = 3;
+	int width = 6;
+}
+public class practice_clas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BOX OBJ1 = new BOX();
+		
+		int volume = OBJ1.height * OBJ1.depth * OBJ1.width;
+		
+		System.out.println(volume);
+		
+		
+	}
+
+}
